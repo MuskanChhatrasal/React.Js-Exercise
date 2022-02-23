7034dea04c57177e42df3cfc53b297500806730e
Muskan <muskanchhatrasal@gmail.com>
2022-02-23 08:45:04 +0530

    Add functionality in a todo app

--- src/Exercises/TodoApp/index.js
+++ src/Exercises/TodoApp/index.js
@@ -2,20 +2,35 @@ import React, { useState } from 'react'
 import './styles.css'
 
 const TodoApp = () => {
-  const [item, setItem] = useState('');
+  const [items, setItems] = useState('');
   const [list, setList] = useState([]);
+  
 
-  function clickHandler(){
-      setList(item)
+  function addItem(item){
+    setList([...list,item])
   }
 
+//   const filterItems = (list, itemId) => list.filter((item)=>item.id === itemId)
+//   function strikeHandler(id){
+//       let updatedList = filterItems(list,id);
+//       console.log(updatedList)
+//   }
   return (
     <div>
         <h2>Todo App</h2>
         <label>Enter Task: 
-            <input type='text' onChange={(e)=>setItem(e.target.value)}></input>
-            <button onClick={clickHandler}>Submit</button>
+            <input type='text' onChange={(e)=>setItems(e.target.value)}></input>
+            <button onClick={()=>addItem(items)}>Submit</button>
         </label>
+
+        <h2>My TODO'S</h2>
+        <ul>
+            {list.map((item, index)=>{
+                return(
+                    <li key={index}>{item}</li>
+                )
+            })}
+        </ul>
     </div>
   )
 }
